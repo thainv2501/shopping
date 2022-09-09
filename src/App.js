4d2a91012b6a2abd407d9e1e0578e39ce189a3bc
White_Bear-2501 <taolanhuthedo2001@gmail.com>
2022-09-09 22:10:36 +0700

    thunk function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { productsActions } from "./reducers/productsSlice";
+import { fetchProducts, productsActions } from "./reducers/productsSlice";
 import Products from "./components/Products/Products";
 import Cart from "./components/Cart/Cart";
 import Nav from "./components/Nav/Nav";
@@ -9,14 +9,9 @@ import Nav from "./components/Nav/Nav";
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.cart.showCart);
-  const fetchProducts = async () => {
-    const respond = await fetch("https://dummyjson.com/products");
-    const data = await respond.json();
-    console.log(data.products);
-    dispatch(productsActions.addToProducts(data.products));
-  };
+
   useEffect(() => {
-    fetchProducts();
+    dispatch(fetchProducts());
     return () => {
       console.log("function clear was run , now run effect");
     };
--- src/reducers/productsSlice.js
+++ src/reducers/productsSlice.js
@@ -11,6 +11,17 @@ const productSlice = createSlice({
     },
   },
 });
-
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const respond = await fetch("https://dummyjson.com/products");
+      const data = await respond.json();
+      console.log(data.products);
+      return data.products;
+    };
+    const products = await fetchData();
+    dispatch(productsActions.addToProducts(products));
+  };
+};
 export const productsActions = productSlice.actions;
 export default productSlice;
